3a4aab3ce2d933ca2e59cf4852ec66df7e1a6189
ricards <ricards.taukuls@inbox.lv>
2025-03-30 11:52:18 +0300

    feature: Added Microsoft sql server in docker compose

---
+++ Store-CRUD-MVC/Services/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Store_CRUD_MVC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        
+    }
+}
