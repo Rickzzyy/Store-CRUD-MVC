f89d935d83c8ccdc7300bd1655e845d32a049934
ricards <ricards.taukuls@inbox.lv>
2025-03-30 14:29:37 +0300

    refactoring: automatic database creation

--- Store-CRUD-MVC/Program.cs
+++ Store-CRUD-MVC/Program.cs
@@ -16,12 +16,25 @@ var app = builder.Build();
 
 
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var db = services.GetRequiredService<ApplicationDbContext>();
+        db.Database.EnsureCreated();
+        db.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
 
 if (!app.Environment.IsDevelopment())
 {
-    using var scope = app.Services.CreateScope();
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
+    
 
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
