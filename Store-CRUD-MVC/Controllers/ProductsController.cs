edc41767a45da9a9c921382b242422bf64301ee9
ricards <ricards.taukuls@inbox.lv>
2025-03-30 13:49:25 +0300

    feature: Create product, save product

--- Store-CRUD-MVC/Controllers/ProductsController.cs
+++ Store-CRUD-MVC/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Store_CRUD_MVC.Models;
 using Store_CRUD_MVC.Services;
 
 namespace Store_CRUD_MVC.Controllers
@@ -6,10 +7,12 @@ namespace Store_CRUD_MVC.Controllers
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext context;
+        private readonly IWebHostEnvironment enviroment;
 
-        public ProductsController(ApplicationDbContext context)
+        public ProductsController(ApplicationDbContext context, IWebHostEnvironment enviroment)
         {
             this.context = context;
+            this.enviroment = enviroment;
         }
         public IActionResult Index()
         {
@@ -19,9 +22,41 @@ namespace Store_CRUD_MVC.Controllers
 
         public IActionResult Create()
         {
-
-
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(ProductDto productDto)
+        {
+            if(productDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "The Image is required");
+            }
+            if(!ModelState.IsValid)
+            {
+                return View(productDto);
+            }
+
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + Path.GetExtension(productDto.ImageFile!.FileName);
+            string imageFullPath = enviroment.WebRootPath + "/products/" + newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                productDto.ImageFile.CopyTo(stream);
+            }
+
+            Product product = new Product
+            {
+                Name = productDto.Name,
+                Brand = productDto.Brand,
+                Category = productDto.Category,
+                Price = productDto.Price,
+                Description = productDto.Description,
+                ImageFileName = newFileName,
+                CreatedAt = DateTime.Now,
+            };
+
+            context.Products.Add(product);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Products");
         }
     }
 }
